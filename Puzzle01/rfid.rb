aad31ae59440aded7b88aded26838ffa00ce97f2
MarcNuenoMontolio <marcnueno@gmail.com>
2021-03-03 21:59:39 +0100

    created Rfid class skeleton

---
+++ Puzzle01/rfid.rb
@@ -0,0 +1,13 @@
+class Rfid...
+    ... ...
+    # return uid in hexa str # return uid in hexa str
+    def read_uid(self): def read_uid
+    ... ...
+    end
+    if __name__ == "__main__": end
+    rf = Rfid...()
+    uid = rf.read_uid() if __FILE__ == $0
+    print(uid) rf = Rfid....new
+    uid = rf.read_uid
+    puts uid
+    end
