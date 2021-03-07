e4173c889033896dbc62564d8fe7398488de6bf3
Marc Nueno <marcnueno@gmail.com>
2021-03-07 21:02:08 +0100

    Finished rfid.rb, fixed read_uid method.

--- Puzzle01/rfid.rb
+++ Puzzle01/rfid.rb
@@ -1,30 +1,31 @@
+require 'ruby-nfc'
 class Rfid
-    require 'ruby-nfc'
+    @@readers = NFC::Reader.all
+    @@debug = false
 
-    # return uid in hexa str
     def read_uid
-        readers = NFC::Reader.all
-        if debug
-            p "Available readers: #{readers}"
+        if @@debug
+            puts "Available readers: #{@@readers}"
         end
-
-        readers[0].poll(IsoDep::Tag, Mifare::Classic::Tag, Mifare::Ultralight::Tag) do |tag|
+        
+        @@readers[0].poll(Mifare::Classic::Tag) do |tag|
             begin
-                if tag
-                    uid = tag
-                    uid.upcase!
-                    tag.processed!
+                uid = tag.to_s.split()[0].upcase
+                if @@debug
+                    puts "#{uid}"
                 end
+                return uid
             rescue Exception => e
                 puts e
             end
         end
     end
+end
 
 
-    if __FILE__ == $0
-        rf = Rfid
-        uid = rf.read_uid
-        puts uid
-    end
+if __FILE__ == $0
+    rf = Rfid. new
+    puts "Please login with your university card"
+    uid = rf.read_uid
+    puts uid
 end
