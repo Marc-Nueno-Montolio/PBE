272810c7a2f439273926d9cb7bcc298887c13f3f
Marc Nueno <marcnueno@gmail.com>
2021-03-08 11:55:30 +0100

    Created OLED_info.rb

---
+++ Config/OLED_info.rb
@@ -0,0 +1,55 @@
+require 'SSD1306'
+require 'socket'
+
+
+
+disp = SSD1306::Display.new(protocol: :i2c, path: '/dev/i2c-1', address: 0x3C, width: 128, height: 64)
+
+
+def scroll_text(disp, str1, str2, i, time)
+	index = 0
+	pos = 9
+	str2 = "  " + str2 + " "
+	i.times do
+		while index != str2.length
+			disp.font_size = 2
+			disp.println str1
+			disp.println ""
+			disp.font_size = 2
+			disp.println str2[index..pos]
+			disp.display!
+			sleep time
+			disp.clear
+			index = index +1
+			pos = pos +1
+		end
+		
+		disp.clear!
+		if index == str2.length
+			index = 0
+			pos = 9
+		end
+	end
+end
+
+ip = Socket.ip_address_list[1].ip_address
+
+while true
+	disp.font_size = 4
+	disp.println " PBE"
+	disp.display!
+	sleep 1
+	disp.clear!
+	
+
+	scroll_text(disp, "IP Address:", ip.to_s , 1, 0.2)
+	
+	scroll_text(disp, "Wireless:", "VodafoneE200" , 1, 0.2)
+
+	scroll_text(disp, "Time:", "19:05" , 1, 0.1)
+
+
+end
+
+
+
